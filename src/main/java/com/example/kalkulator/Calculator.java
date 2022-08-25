20821a86bf7ecf6769ee8a980ff56cae4ec29017
Milosz <milosz.sierakowski@wp.pl>
2022-08-25 20:29:52 +0200

    Second commit

--- src/main/java/com/example/kalkulator/Calculator.java
+++ src/main/java/com/example/kalkulator/Calculator.java
@@ -1,4 +1,15 @@
 package com.example.kalkulator;
 
 public class Calculator {
+
+    public int addTwoInts(int first, int second) {
+        int sumFirstAndSecond = first + second;
+        return sumFirstAndSecond;
+    }
+
+    public int substractTwoInts(int first, int second) {
+        int resultOfSubstractFirstAndSecond = first - second;
+        return resultOfSubstractFirstAndSecond;
+    }
+
 }
--- src/main/java/com/example/kalkulator/KalkulatorApplication.java
+++ src/main/java/com/example/kalkulator/KalkulatorApplication.java
@@ -8,6 +8,12 @@ public class KalkulatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KalkulatorApplication.class, args);
+
+        Calculator calculator = new Calculator();
+        int sum = calculator.addTwoInts(2, 5);
+        int substract = calculator.substractTwoInts(10, 6);
+        System.out.printf("Wynik dodawania 2 + 5 = " + sum + ", Wynik odejmowania 10 - 6 = " + substract);
+
     }
 
 }
